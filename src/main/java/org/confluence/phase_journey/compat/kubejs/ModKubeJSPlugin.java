e036bf5842fab087ddf9f22ee251ab73d1c9d227
XiaoHuNao <shiwenbin2250@gmail.com>
2024-11-10 20:07:18 +0800

    compat kubejs

---
+++ src/main/java/org/confluence/phase_journey/compat/kubejs/ModKubeJSPlugin.java
@@ -0,0 +1,8 @@
+package org.confluence.phase_journey.compat.kubejs;
+
+import dev.latvian.mods.kubejs.event.EventGroupRegistry;
+import dev.latvian.mods.kubejs.plugin.KubeJSPlugin;
+
+public class ModKubeJSPlugin implements KubeJSPlugin {
+
+}
