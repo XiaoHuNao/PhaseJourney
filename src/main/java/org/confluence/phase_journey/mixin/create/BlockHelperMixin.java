4abcaaa671a5d3001bb267d48f52608deaf012ed
westernat <westernat233@163.com>
2025-03-13 22:31:57 +0800

    热修复

---
+++ src/main/java/org/confluence/phase_journey/mixin/create/BlockHelperMixin.java
@@ -0,0 +1,20 @@
+package org.confluence.phase_journey.mixin.create;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import org.confluence.phase_journey.common.phase.PhaseManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Pseudo
+@Mixin(targets = "com.simibubi.create.foundation.utility.BlockHelper", remap = false)
+public abstract class BlockHelperMixin {
+    @WrapOperation(method = "destroyBlockAs", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"))
+    private static BlockState replace(Level instance, BlockPos pos, Operation<BlockState> original) {
+        return PhaseManager.BLOCK.replaceSourceIfLevelNotFinishedPhase(instance, original.call(instance, pos));
+    }
+}
