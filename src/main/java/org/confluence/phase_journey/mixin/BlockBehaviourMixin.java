6201ac174b41235d700a48e8b6c5428f7828e461
westernat <westernat233@163.com>
2025-03-20 21:47:37 +0800

    新三矿更不容易被拿到了

--- src/main/java/org/confluence/phase_journey/mixin/BlockBehaviourMixin.java
+++ src/main/java/org/confluence/phase_journey/mixin/BlockBehaviourMixin.java
@@ -1,17 +1,27 @@
 package org.confluence.phase_journey.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.loot.LootParams;
 import org.confluence.phase_journey.common.phase.PhaseManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+import java.util.function.BiConsumer;
 
 @Mixin(BlockBehaviour.class)
 public abstract class BlockBehaviourMixin {
@@ -25,5 +35,28 @@ public abstract class BlockBehaviourMixin {
     @ModifyVariable(method = "getDestroyProgress", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getDestroySpeed(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)F"), argsOnly = true)
     private BlockState replace(BlockState source, @Local(argsOnly = true) Player player) {
         return PhaseManager.BLOCK.replaceSourceIfPlayerNotReachedPhase(player, source);
+    }
+
+    @Mixin(BlockBehaviour.BlockStateBase.class)
+    public abstract static class BlockStateBaseMixin {
+        @WrapOperation(method = "onExplosionHit", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Block;onExplosionHit(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/Explosion;Ljava/util/function/BiConsumer;)V"))
+        private void replaceState(Block instance, BlockState blockState, Level level, BlockPos pos, Explosion explosion, BiConsumer<ItemStack, BlockPos> biConsumer, Operation<Void> original) {
+            BlockState target = PhaseManager.BLOCK.replaceSourceIfLevelNotFinishedPhase(level, blockState);
+            if (target == blockState) {
+                original.call(instance, blockState, level, pos, explosion, biConsumer);
+            } else {
+                original.call(target.getBlock(), target, level, pos, explosion, biConsumer);
+            }
+        }
+
+        @WrapOperation(method = "getDrops", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/Block;getDrops(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/storage/loot/LootParams$Builder;)Ljava/util/List;"))
+        private List<ItemStack> replaceState(Block instance, BlockState blockState, LootParams.Builder builder, Operation<List<ItemStack>> original) {
+            BlockState target = PhaseManager.BLOCK.replaceSourceIfLevelNotFinishedPhase(builder.getLevel(), blockState);
+            if (target == blockState) {
+                return original.call(instance, blockState, builder);
+            } else {
+                return original.call(target.getBlock(), target, builder);
+            }
+        }
     }
 }
