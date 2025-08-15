4027b961825a2335314decfcbb1d7ea780b8e0cc
westernat <westernat233@163.com>
2025-08-16 00:07:33 +0800

    对F3隐藏

---
+++ src/main/java/org/confluence/phase_journey/mixin/client/DebugScreenOverlayMixin.java
@@ -0,0 +1,23 @@
+package org.confluence.phase_journey.mixin.client;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.components.DebugScreenOverlay;
+import net.minecraft.world.level.block.state.BlockState;
+import org.confluence.phase_journey.common.phase.PhaseManager;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(DebugScreenOverlay.class)
+public abstract class DebugScreenOverlayMixin {
+    @Shadow
+    @Final
+    private Minecraft minecraft;
+
+    @ModifyExpressionValue(method = "getSystemInformation", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"))
+    private BlockState wrap(BlockState original) {
+        return PhaseManager.BLOCK.replaceSourceIfPlayerNotReachedPhase(minecraft.player, original);
+    }
+}
