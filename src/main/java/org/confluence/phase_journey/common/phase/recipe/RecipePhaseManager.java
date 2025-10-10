f70e8361561cb4ee684419ee4f98154945ea8d30
XiaoHuNao <65500205+XiaoHuNao@users.noreply.github.com>
2025-10-10 19:29:24 +0800

    创建配方阶段

---
+++ src/main/java/org/confluence/phase_journey/common/phase/recipe/RecipePhaseManager.java
@@ -0,0 +1,52 @@
+package org.confluence.phase_journey.common.phase.recipe;
+
+import java.util.Map;
+
+import org.confluence.phase_journey.common.attachment.PhaseAttachment;
+import org.confluence.phase_journey.common.phase.PhaseManager;
+import org.confluence.phase_journey.common.phase.PhaseType;
+
+import com.mojang.datafixers.util.Pair;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.level.Level;
+
+public class RecipePhaseManager extends PhaseManager<RecipeLockContext> {
+
+    public static final RecipePhaseManager MANAGER = new RecipePhaseManager();
+
+    public boolean isRestricted(Level level, ServerPlayer player, RecipeHolder<?> recipeHolder) {
+        return isRestricted(level, null, player, recipeHolder);
+    }
+
+    public boolean isRestricted(Level level, BlockPos pos, ServerPlayer player, RecipeHolder<?> recipeHolder) {
+        for (Map.Entry<PhaseType, Pair<ResourceLocation, RecipeLockContext>> entry : phaseContexts.entries()) {
+            PhaseType phaseType = entry.getKey();
+            ResourceLocation phase = entry.getValue().getFirst();
+            RecipeLockContext ctx = entry.getValue().getSecond();
+
+            if (!ctx.getSupportedPhaseTypes().contains(phaseType)) {
+                continue;
+            }
+
+            if (recipeHolder.value().getType() != ctx.recipeType) {
+                continue;
+            }
+
+            if (!recipeHolder.id().equals(ctx.recipeID)) {
+                continue;
+            }
+
+            PhaseAttachment attachment = phaseType.getPhaseAttachment(level, pos, player);
+            if (attachment == null) {
+                return false;
+            }
+
+            return attachment.ifPhaseAbsent(phase, () -> true);
+        }
+        return false;
+    }
+}
