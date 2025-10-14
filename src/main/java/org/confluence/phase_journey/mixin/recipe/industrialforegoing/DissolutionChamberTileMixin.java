615cb15985942d56d14c09346f6b624f67afaf12
XiaoHuNao <65500205+XiaoHuNao@users.noreply.github.com>
2025-10-14 22:36:08 +0800

    工业先锋 : 溶解室（dissolution_chamber）液体提取器（fluid_extractor）镭射钻孔（laser_drill_ore）流体镭射钻孔 (laser_drill_fluid)   造石加工（stonework_generate）

---
+++ src/main/java/org/confluence/phase_journey/mixin/recipe/industrialforegoing/DissolutionChamberTileMixin.java
@@ -0,0 +1,39 @@
+package org.confluence.phase_journey.mixin.recipe.industrialforegoing;
+
+import com.buuz135.industrial.block.core.tile.DissolutionChamberTile;
+import com.buuz135.industrial.module.ModuleCore;
+import com.buuz135.industrial.recipe.DissolutionChamberRecipe;
+import com.google.common.collect.Multimap;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.Level;
+import org.confluence.phase_journey.common.phase.recipe.RecipePhaseManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = DissolutionChamberTile.class, remap = false)
+public class DissolutionChamberTileMixin {
+
+    @Shadow
+    private DissolutionChamberRecipe currentRecipe;
+
+    @Inject(method = "checkForRecipe" ,at = @At("RETURN"))
+    private void phasejourney$checkForRecipe(CallbackInfo ci){
+        if(this.currentRecipe == null) return;
+        DissolutionChamberTile blockEntity = (DissolutionChamberTile) (Object) this;
+        Level level = blockEntity.getLevel();
+        if (level != null) {
+            Multimap<RecipeType<?>, RecipeHolder<?>> recipes = level.getRecipeManager().byType;
+            for (RecipeHolder<?> holder : recipes.get(ModuleCore.DISSOLUTION_TYPE.get())) {
+                if (holder.value() == currentRecipe) {
+                    if (RecipePhaseManager.MANAGER.isRestricted(level, blockEntity.getBlockPos(), currentRecipe.getType(), holder.id())){
+                        this.currentRecipe = null;
+                    }
+                }
+            }
+        }
+    }
+}
